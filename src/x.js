4636337435c8d289c62a34cf03905228258a7270
Lily <2542101862@qq.com>
2022-06-21 16:31:39 +0800

    update

--- src/x.js
+++ src/x.js
@@ -1,1 +1,2 @@
+import from './x.css'
 export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: '[name].[contenthash].js'
-  }
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "小猪佩奇",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
+
