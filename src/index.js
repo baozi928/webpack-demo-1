9f2cb1ec690a1118c54d6d150427958d808df945
Lily <2542101862@qq.com>
2022-06-22 14:02:37 +0800

    ready

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import x from './x.js'
-console.log(x);
+import jpg from './assets/小猫.jpg'
+const div = document.getElementById('app')
+console.log(jpg);
+div.innerHTML = `
+<img src="${jpg}">`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+       const fn = module.default
+       fn()
+    },()=>{
+        console.log('加载错误');
+    })
+}
+div.appendChild(button)
